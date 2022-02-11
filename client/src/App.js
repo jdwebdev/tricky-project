e3f774d1fdcd76c034f8fe3a4a9dbc7333410449
jdwebdev <jdwebdev974@gmail.com>
2022-02-11 16:44:40 +0100

    Add first tests and create game component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Game from './components/Game';
+
 
 const App = () => {
+
+  const [gameState, setGameState] = React.useState('start');
+
+  const handleClick = () => {
+    setGameState("game");
+    // console.log(gameState)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          <h1>Tricky Project</h1>
       </header>
+      {gameState === "start" && 
+        <div className="startBtnContainer"> 
+          <button className="startBtn" onClick={handleClick}>Start</button>
+        </div>
+      }
+      {gameState === "game" &&
+        <Game />
+      }
     </div>
   );
 }
---
+++ client/src/components/Game.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Game = () => {
+  return (
+    <div className="gameContainer">
+        
+    </div>
+  );
+};
+
+export default Game;
