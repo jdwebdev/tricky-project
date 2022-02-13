55418b9336c9c1f5c80a9bf8eacdd6deeab47b43
jdwebdev <jdwebdev974@gmail.com>
2022-02-13 12:48:05 +0100

    Add Redux logic

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -2,39 +2,57 @@ import React, { useEffect } from 'react';
 import Tile from './Tile';
 import { useState } from 'react';
 import emptyTile from '../images/empty_tile.png';
-import data from '../data'
 import player from '../images/player.png';
 import pharmacie from '../images/pharmacie.png';
+import gel from '../images/gel.png';
 import noMask from '../images/no_mask.png';
-// import noMaskToMask from '../images/no_mask_to_mask.png';
-// import virus from '../images/virus.png';
+import mask from '../images/mask.png';
+import virus from '../images/virus.png';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { nextStage, movePlayer, giveMask } from '../actions/gameState';
 
 const boardWidth = 9; 
 
 const Game = () => {
-
+    
+    const dispatch = useDispatch();
+    const currentStage = useSelector(state => state);
     const [tileList, setTileList] = useState([]);
-    const [currentStage, setCurrentStage] = useState(0);
+    const [actionBtnLabel, setActionBtnLabel] = useState("Aucune action");
+    const [actionId, setActionId] = useState(-1);
 
     useEffect(() => {
-        createTileList();
-        setCurrentStage(0);
-    }, []);
+        createTileList(currentStage);
+    }, [currentStage]);
 
-    const createTileList = () => {
+    useEffect(() => {
+        checkAdjacencyTiles();
+    }, [tileList]);
+
+    const createTileList = (currentStage) => {
         let newList = []
         let imgSrc = emptyTile;
         let tileType = "empty";
         for (let i = 0; i < boardWidth**2; i++) {
-            if (i === data.stage[currentStage].playerPos) {
+            if (i === currentStage.playerPos) {
                 imgSrc = player;
                 tileType = "player";
-            } else if (i === data.stage[currentStage].pharmacie) {
+            } else if (i === currentStage.pharmacie) {
                 imgSrc = pharmacie;
                 tileType = "pharmacie";
-            } else if (data.stage[currentStage].noMask.includes(i)){ 
+            } else if (currentStage.noMask.includes(i)){ 
                 imgSrc = noMask;
                 tileType = "noMask";
+            } else if (i === currentStage.gel) {
+                imgSrc = gel;
+                tileType = "gel";
+            } else if (currentStage.mask.includes(i)) {
+                imgSrc = mask;
+                tileType = "mask";
+            } else if (currentStage.virus.includes(i)) {
+                imgSrc = virus;
+                tileType = "virus";
             } else {
                 imgSrc = emptyTile;
                 tileType = "empty"
@@ -50,15 +68,128 @@ const Game = () => {
         setTileList(newList)
     }
 
-  return (
-    <div className="mainContainer">
-        <div className="boardContainer">
-            { tileList.length > 0 &&
-                tileList.map(t => t)
+    const move = (direction) => {
+
+        // Check if the tileId is valid
+        let directionTileId;
+        const playerPosition = currentStage.playerPos;
+        switch(direction) {
+            case "up":
+                directionTileId = playerPosition - boardWidth;
+                if (playerPosition < boardWidth) return;
+                break;
+            case "down":
+                directionTileId = playerPosition + boardWidth;
+                if (playerPosition >= (boardWidth**2 - boardWidth)) return;
+                break;
+            case "right":
+                directionTileId = playerPosition + 1;
+                if((directionTileId % boardWidth === 0) && playerPosition === directionTileId - 1) return;
+                break;
+            case "left":
+                directionTileId = playerPosition - 1;
+                if(playerPosition === 0 || playerPosition % boardWidth === 0) return;
+                break;
+            default:
+                return;
+        }
+
+        if (tileList[directionTileId].props.type === "empty") {
+            dispatch(movePlayer(directionTileId));
+        }
+    }
+
+    const checkAdjacencyTiles = () => {
+        const playerPosition = currentStage.playerPos;
+        const topTile = playerPosition - boardWidth;
+        const bottomTile = playerPosition + boardWidth;
+        const rightTile = playerPosition + 1;
+        const leftTile = playerPosition -1;
+
+        if (tileList?.[topTile]) {
+            if (tileList[topTile].props.type !== "empty") {
+                activeActionButton(tileList[topTile].props.type, topTile);
+                return;
             }
+        }
+        if (tileList?.[bottomTile]) {
+            if (tileList[bottomTile].props.type !== "empty") {
+                activeActionButton(tileList[bottomTile].props.type, bottomTile);
+                return;
+            }
+        }
+        if (tileList?.[rightTile]) {
+            if (tileList[rightTile].props.type !== "empty") {
+                activeActionButton(tileList[rightTile].props.type, rightTile);
+                return;
+            }
+        }
+        if (tileList?.[leftTile]) {
+            if (tileList[leftTile].props.type !== "empty") {
+                activeActionButton(tileList[leftTile].props.type, leftTile);
+                return;
+            }
+        }
+        if (actionBtnLabel !== "Aucune action") {
+            setActionBtnLabel("Aucune action", -1);
+        }
+    }
+
+    const activeActionButton = (type, tileId) => {
+        switch(type) {
+            case "noMask":
+                setActionBtnLabel("Donner un masque");
+                setActionId(tileId);
+                break;
+            case "pharmacie":
+                setActionBtnLabel("Entrer dans la pharmacie");
+                setActionId(tileId);
+                break;
+            case "gel":
+                setActionBtnLabel("Prendre du gel");
+                setActionId(tileId);
+                break;
+            default: 
+                setActionBtnLabel("Aucune action");
+                setActionId(tileId);
+        }
+    }
+
+    const doAction = (label) => {
+        switch(label) {
+            case "Donner un masque":
+                dispatch(giveMask(actionId));
+                setActionBtnLabel("Aucune action");
+                setActionId(-1);
+                break;
+            case "Entrer dans la pharmacie":
+                break;
+            case "Prendre du gel":
+                break;
+            case "Aucune action":
+                break;
+            default: 
+                return;
+        }
+    }
+
+    return (
+        <div className="mainContainer">
+            <div className="boardContainer">
+                { tileList.length > 0 &&
+                    tileList.map(t => t)
+                }
+            </div>
+            <div className="moveBtnContainer">
+                <button onClick={() => move("up")}>▲</button>
+                <button onClick={() => move("down")}>▼</button>
+                <button onClick={() => move("left")}>◄</button>
+                <button onClick={() => move("right")}>►</button>
+            </div>
+            <button onClick={() => dispatch(nextStage(currentStage.number+1))}>NEXT</button>
+            <button onClick={() => doAction(actionBtnLabel)}>{actionBtnLabel}</button>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Game;
